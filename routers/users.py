246eaf7b3c6ae2a643db29abfb105189fc790b01
JoshuaGanz <jravaellnew@gmail.com>
2024-12-31 21:58:43 +0700

    Modified some response

--- routers/users.py
+++ routers/users.py
@@ -1,14 +1,20 @@
 from fastapi import APIRouter
 from fastapi.responses import Response, JSONResponse
 from models.user import User 
+from bot import user_accounts
 from passlib.hash import pbkdf2_sha256 as sha256
-from controllers import users
 
 router = APIRouter(prefix='/users')
 
 @router.post('/login')
 async def getUser(user: User):
-    users.getUser(user)
+    for user_id, user_data in user_accounts.items():
+        if user_data['username'] == user.username and sha256.verify(user.password, user_data['password']):
+            user = user_accounts[user_id]
+            user['userId'] = user_id
+            print(user)
+            return JSONResponse(user, status_code=200)
+    return JSONResponse({ 'msg': 'Invalid username or password' }, status_code=401)
     
 
 @router.post('test')
