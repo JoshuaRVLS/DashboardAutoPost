6d4a0362200d51e3228f459beaeb1c64579197d9
Joshua Ravael <jravaellnew@gmail.com>
2025-01-02 19:42:30 +0700

    Adding Navbar and Renew Code System

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,8 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 export const metadata = {
   title: "User Dashboard",
   description: "User dashboard",
 };
 
 export default function DashboardLayout({ children }) {
-  return children;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,14 +2,24 @@
 
 import React from "react";
 import { signOut, useSession } from "next-auth/react";
+import RenewCode from "@/components/RenewCode";
 
 const page = () => {
   const { data: session } = useSession();
 
   return (
     <div className="center">
-      <div className="form-col">
-        {session?.user?.username} | userID: {session?.user?.userId}
+      <div className="form-col max-w-fit">
+        {session?.user?.expired ? (
+          <RenewCode />
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <span>
+              Subscription <span className="text-green-400">Active</span>
+            </span>
+            <span>Expired Date: {session?.user?.expiry}</span>
+          </div>
+        )}
         <button className="btn" onClick={signOut}>
           sign out
         </button>
