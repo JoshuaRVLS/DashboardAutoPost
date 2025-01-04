22099647db1996dccbc9b234440704995e6bbfed
Joshua Ravael <jravaellnew@gmail.com>
2025-01-04 23:46:11 +0700

    Adding account provider for account selection

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar/Navbar";
+import AccountProvider from "@/providers/AccountProvider";
 
 export const metadata = {
   title: "User Dashboard",
@@ -7,9 +8,9 @@ export const metadata = {
 
 export default function DashboardLayout({ children }) {
   return (
-    <>
+    <AccountProvider>
       <Navbar />
       {children}
-    </>
+    </AccountProvider>
   );
 }
