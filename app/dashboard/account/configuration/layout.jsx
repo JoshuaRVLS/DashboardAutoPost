cf68d7b00093adb8440c39583283c481257238d0
Joshua Ravael <jravaellnew@gmail.com>
2025-01-09 21:07:51 +0700

    Change route name

---
+++ app/dashboard/account/configuration/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Account Configuration",
+  description: "Account configuration",
+};
+
+export default function DashboardLayout({ children }) {
+  return children;
+}
